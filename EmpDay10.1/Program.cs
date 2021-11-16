3265c7af7056197fe06b330f0e6afa3904b03b87
naveen <naveenreddy.k07@gmail.com>
2021-11-16 18:48:28 +0530

    first commit

---
+++ EmpDay10.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmpDay10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if(empCheck==isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadKey();
+        }
+    }
+}
