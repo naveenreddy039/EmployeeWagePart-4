f610acc0ecdc9d205293ff6e4658af7c61798a73
naveen <naveenreddy.k07@gmail.com>
2021-11-16 20:25:24 +0530

    first commit

---
+++ EmpDay10.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EmpDay10._4
+{
+    class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHr = 20;
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(1, 2);
+            switch (empCheck)
+            {
+                case isPartTime:
+                    empHrs = 8;
+                    break;
+                case isFullTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * empRatePerHr;
+            Console.WriteLine(empWage);
+        }
+    }
+}
