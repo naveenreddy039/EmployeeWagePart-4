f0033bd67970eb05670492994916143b8b4fb7e1
naveen <naveenreddy.k07@gmail.com>
2021-11-16 20:28:12 +0530

    first commit

---
+++ EmpDay10.5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EmpDay10._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPartTime = 1;
+            int isFullTime = 2;
+            int maxHoursInMonth = 20;
+            int maxWorkingDaysInMonth = 20;
+            int empRatePerDay = 20;
+
+            int totalEmpHrs = 0;
+            int totalWorkingHors = 0;
+            int workingHrs;
+
+            Random random = new Random();
+            int empCheck = random.Next(1, 2);
+            if (empCheck == isFullTime)
+            {
+                workingHrs = 8;
+            }
+            else if (empCheck == isPartTime)
+            {
+                workingHrs = 4;
+            }
+            else
+            {
+                workingHrs = 0;
+            }
+            int totalWorkingHrs = workingHrs * maxHoursInMonth;
+            int totalSalary = totalWorkingHrs * empRatePerDay;
+            Console.WriteLine(totalSalary);
+        }
+    }
+}
