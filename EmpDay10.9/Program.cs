0cae484ae73d26e9c1248f1b2163508f5c69d2c9
naveen <naveenreddy.k07@gmail.com>
2021-11-16 20:37:46 +0530

    first commit

---
+++ EmpDay10.9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpDay10._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
