2e46f5881450f418a7b75bfd861f4806c65de95e
naveen <naveenreddy.k07@gmail.com>
2021-11-16 19:44:00 +0530

    first commit

---
+++ EmpDay10.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmpDay10._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPartTime = 1;
+            int partTimeWagePerHr = 8;
+
+            int partTime = 0;
+            int partTimeEmpWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(1, 2);
+            if (empCheck == isPartTime)
+            {
+                partTime = 4;
+            }
+            else
+            {
+                partTime = 0;
+            }
+            partTimeEmpWage = partTime * partTimeWagePerHr;
+            Console.WriteLine("parttime emp wage:" + partTimeEmpWage);
+            Console.ReadKey();
+        }
+    }
+}
