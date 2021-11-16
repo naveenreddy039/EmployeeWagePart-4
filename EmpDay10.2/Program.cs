1b047bf21ea069a47505157e7c1043ce371227e9
naveen <naveenreddy.k07@gmail.com>
2021-11-16 18:56:29 +0530

    first commit

---
+++ EmpDay10.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmpDay10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            int empRatePerHour = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(1, 2);
+            if (empCheck == isFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Empoly Days Wage" + empWage);
+            Console.ReadKey();
+        }
+    }
+}
